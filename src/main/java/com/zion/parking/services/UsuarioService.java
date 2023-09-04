af09dfb503e414b1ba222f5f4e060d5cf1f4c3e6
Alexandre Ferreira <ajferreira.br@hotmail.com>
2023-09-03 21:50:40 -0300

    update usuario controller e usuario service

--- src/main/java/com/zion/parking/services/UsuarioService.java
+++ src/main/java/com/zion/parking/services/UsuarioService.java
@@ -1,9 +1,13 @@
 package com.zion.parking.services;
 
+import com.zion.parking.entities.Usuario;
 import com.zion.parking.repositories.UsuarioRepository;
+import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 
 @Service
@@ -13,8 +17,14 @@ public class UsuarioService {
     private UsuarioRepository repository;
 
 
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return repository.save(usuario);
+    }
 
-
-
-
+    @Transactional(readOnly = true)
+    public Usuario findById(Long id) {
+        return repository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuario não encontrado") );
+    }
 }
--- src/main/java/com/zion/parking/web/controllers/UsuarioController.java
+++ src/main/java/com/zion/parking/web/controllers/UsuarioController.java
@@ -1,10 +1,14 @@
 package com.zion.parking.web.controllers;
 
 
+import com.zion.parking.entities.Usuario;
 import com.zion.parking.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/usuarios")
@@ -12,6 +16,20 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioService service;
+
+    @PostMapping
+    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario) {
+        Usuario user = service.salvar(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> findById(@PathVariable Long id) {
+        Usuario user = service.findById(id);
+        return ResponseEntity.ok(user);
+    }
+
+
 
 
 
